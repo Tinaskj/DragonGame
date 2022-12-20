9927c75cf6a86d867b6323cdb64d04626e03da57
Tinaskj <106248161+Tinaskj@users.noreply.github.com>
2022-12-20 15:37:05 +0100

    First commit

---
+++ js/desertFindController.js
@@ -0,0 +1,4 @@
+function goToDesertFind(){
+    model.currentPage = "desertFind"
+    updateView()
+}
---
+++ js/explorerController.js
@@ -0,0 +1,4 @@
+function goToExplorer() {
+    model.currentPage = "explorer"
+    updateView();
+}
